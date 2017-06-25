6b254977b809cd2d0b0082bc7277191db7948f36
VVoev <vladovoev@yahoo.co.uk>
2017-06-25 16:01:35 +0300

    application is now shit

--- Crawler/parsers/area.parser.js
+++ Crawler/parsers/area.parser.js
@@ -1,6 +1,6 @@
-const { Area } = require('../models/area');
-const { Selectors } = require('../selectors');
-const { parseLandmark } = require('./landmark.parser');
+const {Area} = require('../models/area');
+const {Selectors} = require('../selectors');
+const {parseLandmark} = require('./landmark.parser');
 const initDomParser = require('../dom-parser');
 
 const REQUEST_SPEED = 3000;
@@ -23,16 +23,48 @@ const parseAreas = (url) => {
                 $elements.each((_, elem) => {
                     const $elem = $(elem);
                     const href = $elem.attr(Selectors.href).split('/')[3];
-                    if (href === Selectors.item){ 
+                    if (href === Selectors.item) {
                         return;
                     }
-                  
+
+                    //new node
+
                     const landmarkUrl = Selectors.baseurl + area.id + "/" + href;
                     parseLandmark(landmarkUrl)
                         .then((landmark) => {
+                            console.log(landmark.pictureUrl)
                             area.landmarksIds.push(landmark);
+                            var fs = require('fs'),
+                                request = require('request'),
+                            path = require('path');
+//q sloji 1 path
+                            var download = function (uri, filename, callback) {
+                                request.head(uri, function (err, res, body) {
+                                    console.log('content-type:', res.headers['content-type']);
+                                    console.log('content-length:', res.headers['content-length']);
+
+                                    request(uri)
+                                        .pipe(fs.createWriteStream(path.join(__dirname, filename)))
+                                        .on('close', callback);
+                                });
+                            };
+
+                            //sample code
+                            // 'http://100nto.org/media/k2/items/cache/c9b002fe1bb0320831a8ae78670fdb6f_L.jpg', 'ba.jpg'
+
+                                    //url     // toq download tuka li trqbva da stoi ?? mi proprincip moje da e
+                            // po nagore v scope-a samata funkciq ma ne bi trqbvalo da e problem sega tva
+                            // iskash li da ti go pushna i az shte se connectna? 
+                            // izlez 1 direktoriq nazad             filename
+                            download(landmark.pictureUrl, landmark.title+'.jpeg', function () {
+                                console.log('done');
+                            });
                         });
-                });
+
+                    // imaha li title landmarcite :D  da emi q taka ne che koi znae kvo razlichno de ama
+                });//vij 4e tuka iska da suzdade nqkva nesushtestvuvashta direktoriq?
+                //to mai ochakva ime na fail a nie mu davame direktoriq mai tva e problema mi qvno da
+                // da ne bi da trqbva putishtata prez toq path obekt da se pravqt
             });
     };
 
