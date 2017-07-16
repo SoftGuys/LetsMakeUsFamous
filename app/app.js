a17700319686bc41e9bcb606802d7a751576a637
Rosen Urkov <rosen.urkov@gmail.com>
2017-07-16 16:26:51 +0300

    Set up flash and split app-configurations

--- app/app.js
+++ app/app.js
@@ -1,11 +1,18 @@
+/* globals __dirname */
+
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
 const init = (data) => {
     require('./config/app.config')(app);
     require('./config/auth.config')(app, data);
-    const controllers = require('./controllers')(data);
 
+    app.use('/static', express.static(path.join(__dirname, '../public')));
+    app.use('/libs', express.static(path.join(__dirname, '../node_modules')));
+
+    const controllers = require('./controllers')(data);
     require('./routes')(app, controllers);
 
     return Promise.resolve(app);
