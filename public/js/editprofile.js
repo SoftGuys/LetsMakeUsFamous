4ed52afeaeb974345af39017adcaedc425d623b5
VVoev <vladovoev@yahoo.co.uk>
2017-07-21 00:04:19 +0300

    fixed edit profile and remove home.js

--- public/js/editprofile.js
+++ public/js/editprofile.js
@@ -1,30 +1,34 @@
 /* globals $ */
 $('document').ready(() => {
     const button = $('#buttonEditProfile');
-    const editable = $('.edittable');
     const save = $('#saveButton');
     save.hide();
     button.show();
 
 
-    button.on('click', function() {
+    button.on('click', function(event) {
+        event.preventDefault();
+        const editable = $('.edittable');
         button.hide();
         save.show();
+        console.log('editvam');
         for (const i of editable) {
-            console.log(i);
+            console.log(i)
             // eslint-disable-next-line
             $(i).replaceWith($(`<input value="${$(i).text()}">`).addClass('form-control').css('background-color', '73ff00'));
         }
     });
 
-        save.on('click', function() {
-            const formControl = $('.form-control');
-            save.hide();
-            button.show();
-            for (const j of formControl) {
-                console.log(j);
-                // eslint-disable-next-line
-                $(j).replaceWith($(`<td>${$(j).val()}</td>`));
-            }
-        });
+    save.on('click', function(event) {
+        event.preventDefault();
+        const formControl = $('.form-control');
+        save.hide();
+        button.show();
+        console.log(formControl);
+        for (const j of formControl) {
+            console.log(j);
+            // eslint-disable-next-line
+            $(j).replaceWith($(`<td>${$(j).val()}</td>`).addClass('edittable'));
+        }
+    });
 });
--- public/js/home.js
+++
@@ -1,8 +0,0 @@
-/* globals $*/
-
-window.onload = function() {
-    const $xui = $('#explore');
-    $xui.css('color', 'black').css('opacity', '0.4');
-};
-
-
