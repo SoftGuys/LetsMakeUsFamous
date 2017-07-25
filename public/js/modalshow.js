1c357b96a6c90d890563a14fe799870194b023ed
VVoev <vladovoev@yahoo.co.uk>
2017-07-25 21:28:02 +0300

    now the googlemarker is always in the center

--- public/js/modalshow.js
+++ public/js/modalshow.js
@@ -4,6 +4,8 @@ const modalContext = $('#myModal');
 mapBtn.on('click', function() {
     $('#myModal').modal('show');
     setTimeout(()=>{
+        const longitude = Number($('#cordslong').attr('data-lon'));
+        const latitude = Number($('#cordslat').attr('data-lat'));
         $('#address').hide();
         $('#submit').hide();
         $('#floating-panel').hide();
@@ -13,6 +15,16 @@ mapBtn.on('click', function() {
         $('.modal-footer').css('background-color', 'black');
         $('.btn-default').css('background-color', '#6a0f0e');
         $('.btn-default').css('background-color', '#ead0af');
-        google.maps.event.trigger(map, 'resize');
+        const map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 9,
+            center: { lat: latitude, lng: longitude },
+        });
+        const marker = new google.maps.Marker({
+            map: map,
+            position: map.center,
+            // Animation can be DROP,BOUNCE,no,po
+            animation: google.maps.Animation.BOUNCE,
+        });
+        // google.maps.event.trigger(map, 'resize');
     }, 300);
 });
