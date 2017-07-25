6699ba4a05d7dc6f13c5aa78bce8618e82e2b7c4
VVoev <vladovoev@yahoo.co.uk>
2017-07-25 20:19:34 +0300

    fixed modal

--- public/js/modalshow.js
+++ public/js/modalshow.js
@@ -1,6 +1,18 @@
-/* globals  $ */
+/* globals $ google map*/
 const mapBtn = $('#googleMapActivate');
 const modalContext = $('#myModal');
 mapBtn.on('click', function() {
     $('#myModal').modal('show');
+    setTimeout(()=>{
+        $('#address').hide();
+        $('#submit').hide();
+        $('#floating-panel').hide();
+        $('.modal-header').css('background-color', 'black');
+        $('.modal-header').css('color', '#ead0af');
+        $('.modal-body').css('background-color', 'black');
+        $('.modal-footer').css('background-color', 'black');
+        $('.btn-default').css('background-color', '#6a0f0e');
+        $('.btn-default').css('background-color', '#ead0af');
+        google.maps.event.trigger(map, 'resize');
+    }, 300);
 });
