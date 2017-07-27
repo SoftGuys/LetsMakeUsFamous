60aeb418e7e54d13c5223603a996baf00905d5a4
Borislav Borisov <borislavborisov22@abv.bg>
2017-07-27 19:54:03 +0300

    added ajax requests wrapper

---
+++ public/js/requests/requester.js
@@ -0,0 +1,47 @@
+/* globals $ */
+const requester = (() => {
+    const request = (url,
+        method,
+        contentType = '',
+        body = {},
+        headers = {}) => {
+        const promise = new Promise((resolve, reject) => {
+            $.ajax({
+                url: url,
+                type: method,
+                data: JSON.stringify(body),
+                contentType: contentType,
+                headers: headers,
+                success: (data) => resolve(data),
+                error: (err) => reject(err),
+            });
+        });
+
+        return promise;
+    };
+
+    const putJSON = (url, body, headers = {}) => {
+        return request(url, 'PUT', 'application/json', body, headers);
+    };
+
+    const postJSON = (url, body, headers = {}) => {
+        return request(url, 'POST', 'application/json', body, headers);
+    };
+
+    const getJSON = (url, headers = {}) => {
+        return request(url, 'GET', 'application/json', {}, headers);
+    };
+
+    const patchJSON = (url, body = {}, headers = {}) => {
+        return request(url, 'PATCH', 'application/json', body, headers);
+    };
+
+    return {
+        postJSON,
+        putJSON,
+        getJSON,
+        patchJSON,
+    };
+})();
+
+console.log(requester.postJSON);
