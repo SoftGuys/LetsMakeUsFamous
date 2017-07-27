3e6e6b1c5f2fe2d72541f5c9e520a76d4ed7d957
Rosen Urkov <rosen.urkov@gmail.com>
2017-07-27 19:16:35 +0300

    Setup gulp concat-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,68 @@
 const gulp = require('gulp');
 const gulpsync = require('gulp-sync')(gulp);
+
 const babel = require('gulp-babel');
-const clean = require('gulp-clean');
-const nodemon = require('gulp-nodemon');
-const express = require('gulp-express');
 const eslint = require('gulp-eslint');
 
-gulp.task('server', ['build'], () => {
+const express = require('gulp-express');
+const nodemon = require('gulp-nodemon');
+
+const concat = require('gulp-concat');
+const cleanCss = require('gulp-clean-css');
+
+const clean = require('gulp-clean');
+const pump = require('pump');
+const uglify = require('gulp-uglify');
+
+gulp.task('server', ['build-clean'], () => {
     express.run(['server.js']);
 });
 
-gulp.task('compile:lint', () => {
+gulp.task('build:compress-js', () => {
+    pump([
+        gulp.src('public/js/*.js'),
+        uglify(),
+        gulp.dest('build'),
+    ]);
+});
+
+gulp.task('build:concat-css', () => {
+    return gulp.src(['public/css/*.css'])
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build:lint', () => {
     return gulp.src(['**/*.js', '!node_modules/**', '!build/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('compile:transpile', () => {
-    return gulp.src(['public/js/*.js'])
+gulp.task('build:transpile', () => {
+    return gulp
+        .src(['public/js/*.js'])
         .pipe(babel({
             presets: ['es2015'],
         }))
         .pipe(gulp.dest('build'));
 });
 
-gulp.task('compile', [
-    'compile:lint',
-    'compile:transpile',
+gulp.task('build', [
+    'build:lint',
+    'build:transpile',
+    'build:concat-css',
 ]);
 
 gulp.task('clean', () => {
-    return gulp.src('build', { read: false })
+    return gulp
+        .src('build', { read: false })
         .pipe(clean());
 });
 
-gulp.task('build', gulpsync.sync([
+gulp.task('build-clean', gulpsync.sync([
     'clean',
-    'compile',
+    'build',
 ]));
 
 gulp.task('dev', () => {
