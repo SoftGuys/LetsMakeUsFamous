494bffde0957c481fc634029e528f55aef76aeef
Borislav Borisov <borislavborisov22@abv.bg>
2017-07-31 02:37:03 +0300

    Changed db connetion string from local to cloud

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    PORT: 3001,
-    DB_CLOUD_CONNECTION_STRING: `mongodb://user:123456@ds153422.mlab.com:53422/touristcli`,
+    PORT: 80,
+    DB_CLOUD_CONNECTION_STRING: `mongodb://ec2-35-157-141-50.eu-central-1.compute.amazonaws.com:27017/tourist-sites`,
     DB_LOCAL_CONNECTION_STRING: 'mongodb://localhost/tourist-sites',
 };
 
